15d8763c4b12d75a7984de62a4a3a09b411e11c1
heydar <heydar.fahmi@sokhan.ai>
2023-04-04 18:18:34 +0330

    add minhash algorithm

---
+++ src/LSH/MinHash.py
@@ -0,0 +1,47 @@
+
+MAX_NUMBER = 1000000000
+
+
+class MinHash:
+    def __init__(self, Documents=None):
+        self.dynamic_matrix = Documents
+        self.matrix_signature = None
+
+    def sign_dynamic_matrix(self, permutations):
+        '''
+        sign documents using minhash algorithm base on
+        permutations given as input.
+        :param permutations: a list permutations or hashes of words' orders
+        :return: 2D array containing the signature of the class matrix
+        '''
+        matrix_signature = []
+        for column in self.dynamic_matrix:
+            column_signature = []
+            for p in permutations:
+                column_signature.append(
+                    self._advance_sign_column(column, p)
+                )
+            matrix_signature.append(column_signature)
+        self.matrix_signature = matrix_signature
+        return matrix_signature
+
+    def _advance_sign_column(self, column, p):
+        '''
+        sign a column base on min hash algorithm
+        :param column:a list which contains the index of words in document
+        :param p: a list defining universal hashes or permutation
+        :return: signature of column
+        '''
+        #TODO get max NUMBER
+        minhash_p = MAX_NUMBER
+        for i in column:
+            if p[i] < minhash_p:
+                minhash_p = p[i]
+        return minhash_p
+
+
+P = [[1, 2, 3, 4, 0], [3, 0, 2, 4, 1]]
+basicC = [[1, 0, 1, 1, 0], [0, 1, 1, 0, 1]]
+advanceC = [[0, 2, 3], [1, 2, 4]]
+signature = MinHash()
+print(signature.advance_sign_column(advanceC[1], P[0]))
