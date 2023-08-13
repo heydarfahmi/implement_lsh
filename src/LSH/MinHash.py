cce5e1a46d11e634f51400364cf3261cc9fc6645
heydar <heydar.fahmi@sokhan.ai>
2023-08-13 20:56:11 +0330

    update MinHash functions

--- src/LSH/MinHash.py
+++ src/LSH/MinHash.py
@@ -23,16 +23,17 @@ class MinHash:
                 minhash_p = p[i]
         return minhash_p
 
-    def _sign_coordinate_matrix(self, documents, permutations):
+    def _sign_coordinate_matrix(self, coordinate_matrix, permutations):
         '''
         sign documents using minhash algorithm base on
         permutations given as input.
-        :param documents: a vector which contains the nonzero indexs of boolean vector.
+        :param coordinate_matrix: a matrix containing  vectors  which each contains the nonzero index of
+         binary  vector.
         :param permutations: a list permutations or hashes of bit orders
         :return: 2D array containing the signature of the class matrix
         '''
         matrix_signature = []
-        for column in documents:
+        for column in coordinate_matrix:
             column_signature = []
             for p in permutations:
                 column_signature.append(
@@ -41,29 +42,32 @@ class MinHash:
             matrix_signature.append(column_signature)
         return matrix_signature
 
-    def sign_coordinate_matrix(self, documents):
+    def sign_coordinate_matrix(self, coordinate_matrix, p_size):
         '''
        generate n permutations and
        sign documents using minhash algorithm base on them.
-       :param documents: a vector which contains the nonzero indexs of boolean vector.
-       :return: 2D array containing the signature of the class matrix
+       :param coordinate_matrix: a matrix containing  vectors  which each contains the nonzero index of
+         binary  vector.
+        :return: 2D array containing the signature of the class matrix
        '''
         p_generator = RandomPermutation()
-        n=self.n_permutations
-        self._permutations = [p_generator.generate(n) for i in range(self.n_permutations)]
+        self._permutations = [p_generator.generate(p_size) for i in range(self.n_permutations)]
+        print(self._permutations)
         self._matrix_signature = self._sign_coordinate_matrix(
-            documents, self._permutations)
+            coordinate_matrix, self._permutations)
 
         return self._matrix_signature
 
-    def sign_binary_matrix(self, documents):
+    def sign_binary_matrix(self, binary_matrix):
         '''
        generate n permutations and
        sign documents using minhash algorithm base on them.
-       :param documents: matrix containing vectors that each  emphasizes that the vector consists of
+       :param binary_matrix: matrix containing vectors that each  emphasizes that the vector consists of
        only two distinct values, 0 and 1, which can represent binary choices or conditions.
        :return: 2D array containing the signature of the class matrix
        '''
         coordinate_matrix = [list(np.nonzero(document)) for document in documents]
         return self.sign_coordinate_matrix(
-            coordinate_matrix)
+            coordinate_matrix,
+            len(binary_matrix[0])
+        )
